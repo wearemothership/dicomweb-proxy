933385f34d1ead7c41e08723c0b54c5708b8ae4f
Andy Cork <andy.cork@gmail.com>
2022-10-05 08:18:32 +0100

    if get fails, we want an error thrown

--- src/dimse/fetchData.ts
+++ src/dimse/fetchData.ts
@@ -4,23 +4,16 @@ import { fetchMove } from './fetchMove';
 import { ConfParams, config } from '../utils/config';
 import { getLockUid, QUERY_LEVEL } from './querLevel';
 import { Node as DicomNode } from 'dicom-dimse-native';
-import { LoggerSingleton } from '../utils/logger';
 
 const maxAssociations = config.get(ConfParams.MAX_ASSOCIATIONS) as number;
 const throatLock = throat(maxAssociations);
 const lock = new Map();
 
 export async function waitOrFetchData(studyUid: string, seriesUid: string, imageUid: string, level: QUERY_LEVEL): Promise<unknown> {
-  const logger = LoggerSingleton.Instance;
   const peers = config.get(ConfParams.PEERS) as DicomNode[];
 
   for (const peer of peers) {
-    try {
-      return await waitOrFetchDataOnAet(studyUid, seriesUid, imageUid, level, peer);
-    }
-    catch (error) {
-      logger.warn(error);
-    }
+    return await waitOrFetchDataOnAet(studyUid, seriesUid, imageUid, level, peer);
   }
   return Promise.reject('failed waitOrFetchData');
 }
